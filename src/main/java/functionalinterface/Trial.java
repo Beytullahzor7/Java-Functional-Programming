ee6045657f144a8f67197a064571f4a23dd5a713
Beytullahzor7 <beytullahzor7@gmail.com>
2022-02-01 16:57:47 +0300

    FuntionalInterface

---
+++ src/main/java/functionalinterface/Trial.java
@@ -0,0 +1,28 @@
+package functionalinterface;
+
+import java.util.function.*;
+
+public class Trial {
+    public static void main(String[] args) {
+        Function<Integer, Integer> incrementByOneFunction = number -> number + 7;
+
+        Integer a = incrementByOneFunction.apply(4);
+        System.out.println(a);
+
+        BiFunction<Integer,Integer,Integer> addAndMultiply = (num1, num2) -> (num1+5) * num2;
+        System.out.println(addAndMultiply.apply(2,5));
+
+        BiConsumer<String , Boolean> abc = (person, number) -> System.out.println("hello " + person + (number ? 10 : 5));
+        abc.accept("abc", false);
+
+        Predicate<String> isPhoneNumberValid = phoneNumber -> phoneNumber.startsWith("05") && phoneNumber.length() == 11;
+        System.out.println(isPhoneNumberValid.test("05436666666"));
+
+        Supplier<String> getWebAdress = () -> "www.google.com";
+        System.out.println(getWebAdress.get());
+
+
+
+    }
+
+}
